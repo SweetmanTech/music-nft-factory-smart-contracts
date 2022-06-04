a4ba49dbcfc4903457858f1d048f5b2ec166b380
Sweets Sweetman <sweetmantech@gmail.com>
2022-06-04 20:52:53 -0300

    I add configurable Name & Symbol to smart contract.

--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.10;
 import {Album} from "./Album.sol";
 
 contract NFTFactory {
-    function createAlbum() public returns (address) {
-        Album newDrop = new Album("sweetman", "SWTS");
+    function createAlbum(string memory name_, string memory symbol_)
+        public
+        returns (address)
+    {
+        Album newDrop = new Album(name_, symbol_);
         return address(newDrop);
     }
 }
