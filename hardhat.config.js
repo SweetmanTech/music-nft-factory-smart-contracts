2c0f6e51b4899309280319f0abbf0cdcec2d17f1
Sweets Sweetman <sweetmantech@gmail.com>
2022-06-13 16:58:59 -0300

    I setup testnet deployments approved by Zora.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
 
 module.exports = {
   solidity: "0.8.10",
   networks: {
     hardhat: {},
-    polygon: {
-      url: process.env.MATIC_RPC,
-      accounts: [process.env.PRIVATE_KEY],
+    ropsten: {
+      url: process.env.ROPSTEN_RPC,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
     },
-    polygonMumbai: {
-      url: process.env.MUMBAI_RPC,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    goerli: {
-      url: process.env.GOERLI_RPC,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    optimisticKovan: {
-      url: process.env.OPTIMISM_KOVAN_RPC,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    arbitrumTestnet: {
-      url: process.env.ARBITRUM_RINKEBY_RPC,
-      accounts: [process.env.PRIVATE_KEY],
+    rinkeby: {
+      url: process.env.RINKEBY_RPC,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
     },
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.GOERLI_ETHERSCAN_API,
-      optimisticKovan: process.env.OPTIMISM_ETHERSCAN_API,
-      polygon: process.env.POLYGON_ETHERSCAN_API,
-      polygonMumbai: process.env.POLYGON_ETHERSCAN_API,
-      arbitrumTestnet: process.env.ARBITRUM_ETHERSCAN_API,
+      rinkeby: process.env.ETHERSCAN_API_KEY,
+      ropsten: process.env.ETHERSCAN_API_KEY,
     },
   },
 };
